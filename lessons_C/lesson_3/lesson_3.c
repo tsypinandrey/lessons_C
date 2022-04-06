f0e24ee1939875a0ddd4d3e227438be78ccd57ef
tsypinandrey <faxlist@mail.ru>
2022-04-06 19:40:41 +0300

    lesson_3.c

---
+++ lessons_C/lesson_3/lesson_3.c
@@ -0,0 +1,34 @@
+﻿// 29.03.2022
+// Цыпин А.Н.
+//lesson_3.cpp
+//При вводе текста с клавиатуры, все табуляции заменяються на 2 пробела
+
+#include <stdio.h>
+#include <Windows.h>
+#include <limits.h>
+
+int main(int arcs, char* argv[])
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int ch;
+ 
+    printf("Введите несколько предложений:\n");
+    while((ch = getchar()) != EOF)
+    {
+        if ((char)ch == '\t')
+        {
+            (char)ch = ' ';
+            for (int i = 0; i < 2; ++i)
+            {
+                printf("%c", (char)ch);
+            }
+
+        }
+        else {
+            printf("%c", (char)ch);
+        }
+    }
+    return 0;
+}
+
